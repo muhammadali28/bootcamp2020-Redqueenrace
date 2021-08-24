4f97ad19d26698bddd50808d70faedcd32441d7d
muhammadali28 <50048425+muhammadali28@users.noreply.github.com>
2021-08-24 17:50:13 +0500

    Red Queen Race

---
+++ README.md
@@ -0,0 +1,4 @@
+# bootcamp2020-RedQueenRace
+ This is a Complete Project 4A Red Queen Race by using React Hooks
+  
+surgelink: bootcamp2020-redqueenrace.surge.sh 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RedQueenRace } from './RedQueenRace';
+
+function App() {
+  return (
+    <div >
+      <RedQueenRace />
+    </div>
+  );
+}
+
+export default App;
